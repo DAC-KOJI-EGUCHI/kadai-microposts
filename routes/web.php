18f1df0855bd195b1d05bc37b51ab7bc1b8312ce
koji.eguchi <koji.eguchi@dac.co.jp>
2019-05-21 05:53:05 +0000

    Procfile

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,6 @@ Route::group(['middleware' => ['auth']], function(){
         Route::delete('unfollow','UserFollowController@destroy')->name('user.unfollow');
         Route::get('followings', 'UsersController@followings')->name('users.followings');
         Route::get('followers', 'UsersController@followers')->name('users.followers');
-        
     });
     
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
